082f2762d1b86fcdd3c6c93d624cfcec65bee0b8
wassan128 <wassan.taiko.master@gmail.com>
2018-01-18 09:09:49 +0900

    [wip]add draw_calendar

--- js/main.js
+++ js/main.js
@@ -23,23 +23,29 @@ const en_btn = () => {
 	btn_close.addEventListener("click", fn_btn_close, false);
 };
 
-const en_td = () => {
-	const tds = document.getElementsByTagName("td");
-	const fn_tds = (e) => {
-		const now = moment();
+const en_td = (td) => {
+	const fn_td = (e) => {
 		document.querySelector(".modal").style.display = "block";
 	};
-	for (const td of tds) {
-		td.addEventListener("click", fn_tds, false);
-	}
+	td.addEventListener("click", fn_td, false);
 };
 
 const draw_calendar = () => {
+	const st_day = moment().startOf("month").day();
+	const ed_date = moment().endOf("month").date();
+	const tds = document.getElementsByTagName("td");
+	let c = 0;
+	for (const td of tds) {
+		if (c - st_day >= ed_date) break;
+		if (c++ < st_day) continue;
+		td.innerText = c - st_day;
+		en_td(td);
+	}
 };
 
 document.addEventListener("DOMContentLoaded", () => {
 	ld_memo();
 	en_btn();
-	en_td();
+	draw_calendar();
 });
 
