a013dc3989780961663eb7428fbaf6f486021e3e
wassan128 <wassan.taiko.master@gmail.com>
2018-01-19 11:17:05 +0900

    [wip]add memo for each date

--- js/main.js
+++ js/main.js
@@ -9,9 +9,19 @@ const ld_memo = () => {
 const en_btn = () => {
 	const btn_save = document.querySelector(".btn-save");
 	const fn_btn_save = (e) => {
+		const year = document.getElementById("cal-year").innerText;
+		const month = `0${document.getElementById("cal-month").innerText}`.slice(-2);
+		const date = `0${document.getElementById("cal-date").innerText}`.slice(-2);
+
 		const text = document.querySelector("input").value;
-		chrome.storage.local.set({"memo": text}, () => {
-			console.log("success");
+		const key = `${year}${month}${date}`;
+		chrome.storage.local.get(key, (res) => {
+			if (typeof(res[key]) === "undefined") {
+				res[key] = "";
+			}
+			res[key] += `${text};`;
+			chrome.storage.local.set(res, () => {
+			});
 		});
 	};
 	btn_save.addEventListener("click", fn_btn_save, false);
@@ -25,6 +35,8 @@ const en_btn = () => {
 
 const en_td = (td) => {
 	const fn_td = (e) => {
+		const date = e.target.innerText;
+		document.getElementById("cal-date").innerText = date;
 		document.querySelector(".modal").style.display = "block";
 	};
 	td.addEventListener("click", fn_td, false);
@@ -33,12 +45,15 @@ const en_td = (td) => {
 const draw_calendar = () => {
 	const st_day = moment().startOf("month").day();
 	const ed_date = moment().endOf("month").date();
+	
 	const tds = document.getElementsByTagName("td");
 	let c = 0;
 	for (const td of tds) {
-		if (c - st_day >= ed_date) break;
+		const date = c - st_day + 1;
+		if (date > ed_date) break;
 		if (c++ < st_day) continue;
-		td.innerText = c - st_day;
+		td.innerText = date;
+		td.setAttribute("id", "c" + `0${date}`.slice(-2))
 		en_td(td);
 	}
 };
