2011415b8bbe81f4e82c1b298c97c46bb960f840
wassan128 <wassan.taiko.master@gmail.com>
2018-01-19 15:22:10 +0900

    [wip]add memo ary for each dates

--- js/main.js
+++ js/main.js
@@ -1,26 +1,44 @@
 "use strict";
 
+const get_ym = () => {
+	const year = document.getElementById("cal-year").innerText;
+	const month = document.getElementById("cal-month").innerText;
+	return [year, month];
+};
+
 const ld_memo = () => {
-	chrome.storage.local.get("memo", (val) => {
-		console.log(val);
+	const [year, month] = get_ym();
+	chrome.storage.local.get(year, (res) => {
+		for (const date in res[year][month]) {
+			document.getElementById(`c${date}`).style.background = "#c6e48b";
+		}
 	});
 };
 
 const en_btn = () => {
 	const btn_save = document.querySelector(".btn-save");
 	const fn_btn_save = (e) => {
-		const year = document.getElementById("cal-year").innerText;
-		const month = `0${document.getElementById("cal-month").innerText}`.slice(-2);
-		const date = `0${document.getElementById("cal-date").innerText}`.slice(-2);
+		const text = document.querySelector("input");
+		if (text.value === "") {
+			return;
+		}
 
-		const text = document.querySelector("input").value;
-		const key = `${year}${month}${date}`;
-		chrome.storage.local.get(key, (res) => {
-			if (typeof(res[key]) === "undefined") {
-				res[key] = "";
+		const [year, month] = get_ym();
+		const date = document.getElementById("cal-date").innerText;
+
+		chrome.storage.local.get(year, (res) => {
+			if (typeof(res[year]) === "undefined") {
+				res[year] = {};
+				res[year][month] = {};
 			}
-			res[key] += `${text};`;
+			if (typeof(res[year][month][date]) === "undefined") {
+				res[year][month][date] = [text.value];
+			} else {
+				res[year][month][date].push(text.value);
+			}
 			chrome.storage.local.set(res, () => {
+				document.getElementById(`c${date}`).style.background = "#c6e48b";
+				text.value = "";
 			});
 		});
 	};
@@ -35,16 +53,31 @@ const en_btn = () => {
 
 const en_td = (td) => {
 	const fn_td = (e) => {
+		const [year, month] = get_ym();
 		const date = e.target.innerText;
 		document.getElementById("cal-date").innerText = date;
+		chrome.storage.local.get(year, (res) => {
+			const ul = document.querySelector("ul");
+			ul.innerHTML = "";
+			if (typeof(res[year][month][date]) === "undefined") {
+				return;
+			}
+
+			for (const l of res[year][month][date]) {
+				const li = document.createElement("li");
+				li.innerText = l;
+				ul.appendChild(li);
+			}
+		});
 		document.querySelector(".modal").style.display = "block";
 	};
 	td.addEventListener("click", fn_td, false);
 };
 
-const draw_calendar = () => {
-	const st_day = moment().startOf("month").day();
-	const ed_date = moment().endOf("month").date();
+const draw_calendar = (offset) => {
+	const now = moment();
+	const st_day = now.add(offset, "months").startOf("month").day();
+	const ed_date = now.add(offset, "months").endOf("month").date();
 	
 	const tds = document.getElementsByTagName("td");
 	let c = 0;
@@ -53,14 +86,15 @@ const draw_calendar = () => {
 		if (date > ed_date) break;
 		if (c++ < st_day) continue;
 		td.innerText = date;
-		td.setAttribute("id", "c" + `0${date}`.slice(-2))
+		td.setAttribute("id", `c${date}`);
+		td.style.background = "#EEE";
 		en_td(td);
 	}
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-	ld_memo();
 	en_btn();
-	draw_calendar();
+	draw_calendar(0);
+	ld_memo();
 });
 
